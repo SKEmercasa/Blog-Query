b685e36fd7215d798afb76adf747d3308eae7ad5
Slaviya <sergeykiloshenko@list.ru>
2023-05-01 17:58:24 +0300

    dont it  6

--- src/assets/api.js
+++ src/assets/api.js
@@ -5,7 +5,21 @@ const baseURL = 'https://blog.kata.academy/api';
 export async function getArticles(page) {
   const response = await axios.get(`${baseURL}/articles?limit=5&offset=${(page - 1) * 5}`);
   return await response.data;
+}
 
+export async function getUsers() {
+  const response = await axios.post('https://blog.kata.academy/api/users', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    user: {
+      username: 'emercasafront1',
+      email: 'emercasafront1@list.ru',
+      password: 'qwerty2906',
+    },
+  });
+  return await response.data;
 }
 
 export async function getLogin() {
